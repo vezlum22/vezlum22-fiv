c2e7c95f26d49b939e2afea4299d35a7bf4e2315
Luca Vezonik <vezlum22@htl-kaindorf.at>
2025-09-24 13:30:28 +0200

    fiv einheit 2

--- 4ahme/fivu-cool/src/main.cpp
+++ 4ahme/fivu-cool/src/main.cpp
@@ -4,6 +4,9 @@ void setup() {
             //76543210
     PORTB = 0b00100000; //PB5 ein
     DDRB = 0b00100000; //Data direction register 
+            //76543210 
+    PORTB = 0b00110000; //to additionally activate PB4   
+    DDRB = 0b00100000; //Data direction register: output 
 }
 
 void loop() {
