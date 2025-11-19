bc556b68ad3aa043f7071daf8a450b6b1ec87c9a
Luca Vezonik <vezlum22@htl-kaindorf.at>
2025-11-19 13:00:37 +0100

    UART einstellen

--- 4ahme/fivu-cool/src/main.cpp
+++ 4ahme/fivu-cool/src/main.cpp
@@ -1,50 +1,21 @@
-#include <Arduino.h>
+#include <avr/io.h>
 
-void setup() {
-            //76543210
-    PORTB = 0b00100000; //PB5 ein
-    DDRB =  0b00100000; //Data direction register 
-            //76543210 
-    PORTB = 0b00110000; //to additionally activate PB4   
-    DDRB =  0b00110000; //Data direction register: output 
+int main (){
 
-    //Alternative Approach for setting PB4:
-                    //76543210
-    PORTB = PORTB | 0b00010000; //Settings bits always with OR | 
-    //         76543210
-    //PORTB: 0b00110000
-    //       0b00010000 | -> bitwhise or (bitweises ODER)
-    //       0b00110000
+        uint8_t c = 0;
+        //Uart config: 76000/8/N/1
+        //Onboard LED toggles on each character received 
+        DDRB |= (1<<PB5);
+        //UART-Config
+        UBRR0 = 0x0C; //12 for 76800 bit/s with U2X0 = 0
+        UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
 
-    PORTB = PORTB | (1 << PB4);
-    //            76543210
-    //Aim:      0b00010000
-    //(1 << PB4)0b00000001 shifting the "1" 4 times to the back
-    // 1.       0b00000010
-    // 2.       0b00000100
-    // 3.       0b00001000
-    // 4.       0b00010000      
-    PORTB |= (1 << PB4);
-
-    //clearing bits always with AND (&)
-    //             76543210
-    // Current Val.     0b00110000
-    // Bit Mask:        0b11011111
-    // 1.               0b00100000 (1 << PB5)
-    // 2.               0b11011111 ~(1 << PB5)
-    // Aim:             0b00010000 PB5 should be cleared 
-
-    PORTB &= ~(1 << PB5);
-
-    //Button input on PB3
-    //Guarantee PB3 being an input 
-    DDRB &= ~(1 << PB3);
-    //Read an input pin 
-    if(PINB & (1 << PB3)){
-    
-    }
-}
-
-void loop() {
-
+        while(1){
+                if(UCSR0A & (1<<RXC0)){
+                        c = UDR0;
+                        UDR0 =c;
+                        PORTB ^= (1<<PB5); //Toggling with XOR
+                }
+        }
+        return 0;
 }
