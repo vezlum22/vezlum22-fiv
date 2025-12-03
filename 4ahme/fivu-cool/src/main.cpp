365fe3d1ef9ac647385e9c915667df3cd4a9737e
Luca Vezonik <vezlum22@htl-kaindorf.at>
2025-12-03 09:30:02 +0100

    komischer Labo Code

--- 4ahme/fivu-cool/src/main.cpp
+++ 4ahme/fivu-cool/src/main.cpp
@@ -1,8 +1,12 @@
 #include <avr/io.h>
+#include <string.h>
+
+#define MAX_CHARS 80
 
 int main (){
 
-        uint8_t c = 0;
+        char recString[MAX_CHARS] = "";
+        uint8_t charCount = 0;
         //Uart config: 76000/8/N/1
         //Onboard LED toggles on each character received 
         DDRB |= (1<<PB5);
@@ -12,9 +16,30 @@ int main (){
 
         while(1){
                 if(UCSR0A & (1<<RXC0)){
-                        c = UDR0;
-                        UDR0 =c;
-                        PORTB ^= (1<<PB5); //Toggling with XOR
+                        recString[charCount]=UDR0; //RCX0 is set to zero (0) when 09r0 is read
+                        UDR0 = recString[charCount];  //charCount ++ praefix
+
+
+
+                        if (recString[charCount] == '\r' ){
+                                recString[charCount] = 0;
+                                if(strcmp(recString, "TPB5")==0)
+                                        PORTB ^= (1<<PB5); //toggle mit xor    
+                       
+                              
+                        } 
+                        if(++charCount == 80)
+                                charCount = 0; 
+                        
+                        if(recString[charCount-1] == 0x7F){
+                                while (!(UCSR0A & (1<<UDRE0)));
+                                UDR0 = 'A';
+                                charCount -= 2;
+                                recString[charCount] = 0; 
+                                recString[charCount+1] = 0; 
+                        }
+
+                                
                 }
         }
         return 0;
