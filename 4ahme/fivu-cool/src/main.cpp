8ad9308ad2db7776ab4e603c11b409e98d23a2ff
Luca Vezonik <vezlum22@htl-kaindorf.at>
2025-12-03 12:56:31 +0100

    fivu

--- 4ahme/fivu-cool/src/main.cpp
+++ 4ahme/fivu-cool/src/main.cpp
@@ -5,41 +5,18 @@
 
 int main (){
 
-        char recString[MAX_CHARS] = "";
-        uint8_t charCount = 0;
-        //Uart config: 76000/8/N/1
-        //Onboard LED toggles on each character received 
+        int8_t c = 0;
+
         DDRB |= (1<<PB5);
-        //UART-Config
-        UBRR0 = 0x0C; //12 for 76800 bit/s with U2X0 = 0
-        UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
+        //UART Config:
+        UBRR0 = 0x0C;                           //Baud Rate eingestellt
+        UCSR0B |= (1<<RXEN0) | (1<<TXEN0);      //Empfänger und Sender aktiviert
 
-        while(1){
+        while (1){
                 if(UCSR0A & (1<<RXC0)){
-                        recString[charCount]=UDR0; //RCX0 is set to zero (0) when 09r0 is read
-                        UDR0 = recString[charCount];  //charCount ++ praefix
-
-
-
-                        if (recString[charCount] == '\r' ){
-                                recString[charCount] = 0;
-                                if(strcmp(recString, "TPB5")==0)
-                                        PORTB ^= (1<<PB5); //toggle mit xor    
-                       
-                              
-                        } 
-                        if(++charCount == 80)
-                                charCount = 0; 
-                        
-                        if(recString[charCount-1] == 0x7F){
-                                while (!(UCSR0A & (1<<UDRE0)));
-                                UDR0 = 'A';
-                                charCount -= 2;
-                                recString[charCount] = 0; 
-                                recString[charCount+1] = 0; 
-                        }
-
-                                
+                        c = UDR0;
+                        UDR0 = c;
+                        PORTB ^= (1<<PB5);
                 }
         }
         return 0;
