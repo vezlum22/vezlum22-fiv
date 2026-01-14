cea0e19ce0ad0658c76b2708ae0724fb9b89d7d7
Luca Vezonik <vezlum22@htl-kaindorf.at>
2026-01-14 14:00:25 +0100

    fivu

--- 4ahme/fivu-cool/src/main.cpp
+++ 4ahme/fivu-cool/src/main.cpp
@@ -1,62 +1,62 @@
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <avr/interrupt.h>
+#include "timer0.h"
 
-int main(){
-    //Uart config: 76800/8/N/1
-    //Onboard LED toggles on each character received
-    uint8_t c;
-    DDRB |= (1 << PB5);
+int main(){  
+//------------------------------------------------------------------UART Konfiguration-----------------------------
+    // Uart-Config : 76800 bps, 8N1
+    // Onboard-LED toggles on each char received
 
-    //UART Config
-    UBRR0 = 0x0C; //76800 at 16MHz
-    UCSR0B |= (1 << RXEN0) | (1 << TXEN0); //Enable RX and TX
-    UCSR0C |= (1<< UCSZ01) | (1<<UCSZ00);
+    DDRB |= (1 << PB5); // Set pin 13 as output (onboard LED)
+
+    // UART-Config
+    UBRR0 = 0x0C;                           // 76800 baud at 16 MHz
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);  // Enable RX and TX
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 data bits, no parity, 1 stop bit
+
     
-    //Timer1 config for fast PWM
-    DDRB |= (1 << PB1); //Set OC1A (PB1) as output
-    TCCR1A |= (1 << COM1A1) | (1 << WGM11); //Clear OC1A on compare match, set at BOTTOM (non-inverting mode)
-    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11); //Fast PWM mode 14, prescaler 256
-    ICR1 = 39999;  //40000 - 1 -> 20ms
-    OCR1A = 3000;   //4000 -> 2ms
-                    //3000 -> 1,5ms 
-                    //2000 -> 1ms
-                    //1000 -> 0,5ms
-                    //5000 -> 2,5ms 
+//------------------------------------------------------------------Timer1 Konfiguration für servo---------------------
+    DDRB |= (1 << PB1);                                 // Pin 9 (PB1)(Daten Pin vom Servo) = OC1A output
+    TCCR1A = (1 << COM1A1) | (1 << WGM11); // Set OC1A on compare match and WGM11 for Fast PWM
+    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Fast PWM mode with ICR1 as TOP, prescaler = 8
+    ICR1 = 39999; // Set TOP for 50Hz frequency
+    OCR1A = 3000; //4000 -> 2ms
+                    //3000-> 1.5ms
+                    //2000 -> 1.0ms
+                    //1000 -> 0.5ms
+                    //5000 -> 2.5 ms
 
-// Timer0 -> ctc Modus 
-// Period: 125 us
-// Enable Interrupt: TIMER0_COMPB  
+//------------------------------------------------------------------Timer0 Konfiguration für UART Empfang-------------
+    //Timer0 -> CTC mode (Clear Timer on Compare Match)
+    //Period: 125us (8kHz)
+    //Enable Interrupt: Timer0_COMPA
 
-    /*DDRB |= (1 << PB1); //Set OC1A (PB1) as output
-    TCCR1A |= (1 << COM1A0) | (1 << WGM12); //Toggle OC1A on compare match
-    TCCR1A |= (1 << WGM12) | (1 << CS12) | (1 << CS10); //CTC mode
+    timer0CTCInit();
 
-    OCR1A = 15623; //1s at 1024 prescaler
-    */
+    sei(); // Befehl damit globale Interrupts aktiviert werden
+    while (1){
 
-    TCCR0A = (1<<WGM01); 
-    TCCR0B = (1<<CS01);
-    OCR0B = 
-    TIMSK0 = 
-
-    while(1){
-        if(UCSR0A & (1 << RXC0)){ //Check if data received
-            c = UDR0; //Read received data
-            UDR0 = c; //Echo back the received data
-            PORTB &= ~(1 << PB5); //Toggle onboard LED
-            if (c=='l'){
-                OCR1A = 1000;
-            }
-            if (c=='r'){
-                OCR1A = 4000;       //2 clk cycles = 125ns 
-            }
-        }
     }
-   
 
     return 0;
 }
 
-ISR(TIMER0_COMPB_vect){
+//----------------------------------------------------------------Timer0 COMPA Interrupt Service Routine--------------
+// Das wird ausgeführt wenn der Timer0 den wert in OCR0A erreicht hat
+ISR(TIMER0_COMPA_vect){
 
+        uint16_t c = 0;
+    // Wait for data to be received
+        if (UCSR0A & (1 << RXC0)){
+
+            c = UDR0; // Read received byte
+            UDR0 = c; // Echo back
+            PORTB ^= (1 << PB5); // Toggle onboard LED
+            if(c == 'l')
+                OCR1A = 1000;
+            if(c == 'r')
+                OCR1A = 4000;
+            if(c == 'm')
+                OCR1A = 2000;       //2 clock cycles = 125 ns 
+        } 
 }
